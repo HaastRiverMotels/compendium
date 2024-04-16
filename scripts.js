e66d0e39604e24bfcb680219b68cde13f1eb277e
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-04-17 11:49:26 +1200

    Add chevron to collapsible

--- scripts.js
+++ scripts.js
@@ -7,21 +7,26 @@ const enableCollapsibles = () => {
 
   for (const collapsible of collapsibles) {
     collapsible.onclick = () => toggleCollapsible(collapsible);
-
-    // const collapsibleIcon = collapsible.getElementsByClassName("collapsible-icon")
   }
 };
 
 const toggleCollapsible = (collapsible) => {
   collapsible.classList.toggle("active");
 
+  const collapsibleIcon =
+    collapsible.getElementsByClassName("collapsible-icon")[0];
+
   const content = collapsible.nextElementSibling;
   const isOpen = content.style.display === "block";
 
   if (isOpen) {
+    collapsibleIcon.className = collapsibleIcon.className.replace("up", "down");
+
     content.style.display = "none";
     content.style.maxHeight = "0px";
   } else {
+    collapsibleIcon.className = collapsibleIcon.className.replace("down", "up");
+
     content.style.display = "block";
     content.style.maxHeight = content.scrollHeight + "px";
   }
