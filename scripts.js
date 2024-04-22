6e963e9a0efd8981cebe9f7bb59768181cab4193
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-04-23 09:42:40 +1200

    Add activities modal

--- scripts.js
+++ scripts.js
@@ -1,6 +1,37 @@
 const main = () => {
   enableCollapsibles();
   enableTabs();
+  enableModal();
+};
+
+const enableCollapsibles = () => {
+  const collapsibles = document.getElementsByClassName("collapsible");
+
+  for (const collapsible of collapsibles) {
+    collapsible.onclick = () => toggleCollapsible(collapsible);
+  }
+};
+
+const toggleCollapsible = (collapsible) => {
+  collapsible.classList.toggle("active");
+
+  const collapsibleIcon =
+    collapsible.getElementsByClassName("collapsible-icon")[0];
+
+  const content = collapsible.nextElementSibling;
+  const isOpen = content.style.display === "block";
+
+  if (isOpen) {
+    collapsibleIcon.className = collapsibleIcon.className.replace("up", "down");
+
+    content.style.display = "none";
+    content.style.maxHeight = "0px";
+  } else {
+    collapsibleIcon.className = collapsibleIcon.className.replace("down", "up");
+
+    content.style.display = "block";
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
 };
 
 const enableTabs = () => {
@@ -13,14 +44,6 @@ const enableTabs = () => {
   const defaultOpenTabs = document.getElementsByClassName("default-open");
   if (defaultOpenTabs.length > 0) {
     defaultOpenTabs[0].click();
-  }
-};
-
-const enableCollapsibles = () => {
-  const collapsibles = document.getElementsByClassName("collapsible");
-
-  for (const collapsible of collapsibles) {
-    collapsible.onclick = () => toggleCollapsible(collapsible);
   }
 };
 
@@ -46,26 +69,23 @@ const hideAllTabs = () => {
   }
 };
 
-const toggleCollapsible = (collapsible) => {
-  collapsible.classList.toggle("active");
+const enableModal = () => {
+  const modalButton = document.getElementById("modal-button");
+  const modalBackground = document.getElementById("modal-background");
 
-  const collapsibleIcon =
-    collapsible.getElementsByClassName("collapsible-icon")[0];
-
-  const content = collapsible.nextElementSibling;
-  const isOpen = content.style.display === "block";
-
-  if (isOpen) {
-    collapsibleIcon.className = collapsibleIcon.className.replace("up", "down");
-
-    content.style.display = "none";
-    content.style.maxHeight = "0px";
-  } else {
-    collapsibleIcon.className = collapsibleIcon.className.replace("down", "up");
-
-    content.style.display = "block";
-    content.style.maxHeight = content.scrollHeight + "px";
+  if (!modalButton) {
+    return;
   }
+
+  modalButton.onclick = () => {
+    modalBackground.style.display = "block";
+  };
+
+  window.onclick = (event) => {
+    if (event.target === modalBackground) {
+      modalBackground.style.display = "none";
+    }
+  };
 };
 
 main(); // code is ran here
